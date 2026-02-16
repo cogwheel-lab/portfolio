652da9e2b8cb91bb7a74712b4c04df6accbe63ab
cogwheel-lab <220804094+cogwheel-lab@users.noreply.github.com>
2026-02-17 08:13:39 +0900

    feat: Issue 33対応: works配列を定数ファイル化しフッターリンクを修正

---
+++ src/lib/works.js
@@ -0,0 +1,12 @@
+export const works = [
+  {
+    title: "Todoアプリ",
+    description: "Todoアプリの練習",
+    href: "/todo",
+  },
+  {
+    title: "もっと見る",
+    description: "Unsplash APIを使って画像を表示する練習",
+    href: "#unsplash",
+  },
+];
